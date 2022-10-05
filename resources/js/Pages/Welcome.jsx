e67ee959aa20559637c654909f2ceee5f883f1a7
NanaSukarna16 <nana16.sukarna6@gmail.com>
2022-10-05 14:20:51 +0700

    feat: menghapus edit & hapus post di welcome

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -39,61 +39,6 @@ export default function Welcome({ post }) {
                                             Pembuat : {item.user.name}
                                         </span>
                                     </div>
-                                    <div className="flex flex-shrink-0 self-center">
-                                        <Menu as="div" className="relative inline-block text-left">
-                                            <div>
-                                                <Menu.Button className="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600">
-                                                    {/* <span className="sr-only">Open options</span> */}
-                                                    <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
-                                                </Menu.Button>
-                                            </div>
-
-                                            <Transition
-                                                as={Fragment}
-                                                enter="transition ease-out duration-100"
-                                                enterFrom="transform opacity-0 scale-95"
-                                                enterTo="transform opacity-100 scale-100"
-                                                leave="transition ease-in duration-75"
-                                                leaveFrom="transform opacity-100 scale-100"
-                                                leaveTo="transform opacity-0 scale-95"
-                                            >
-                                                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                    <div className="py-1">
-                                                        <Menu.Item>
-                                                            {({ active }) => (
-
-                                                                <Link
-                                                                    href={route("post.edit", item.id)}
-                                                                    className={classNames(
-                                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                                        'flex px-4 py-2 text-sm'
-                                                                    )}
-                                                                >
-                                                                    <PencilSquareIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                                                    <span>Edit</span>
-                                                                </Link>
-                                                            )}
-                                                        </Menu.Item>
-                                                        <Menu.Item>
-                                                            {({ active }) => (
-                                                                <Link
-                                                                    method='delete'
-                                                                    href={route("post.destroy", item.id)}
-                                                                    className={classNames(
-                                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                                        'flex px-4 py-2 text-sm'
-                                                                    )}
-                                                                >
-                                                                    <TrashIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                                                    <span>Hapus</span>
-                                                                </Link>
-                                                            )}
-                                                        </Menu.Item>
-                                                    </div>
-                                                </Menu.Items>
-                                            </Transition>
-                                        </Menu>
-                                    </div>
                                 </div>
 
                             </div>
